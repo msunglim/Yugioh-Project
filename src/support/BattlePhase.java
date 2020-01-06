68520b465a1d939b20471b214a6caeac7006711d
msunglim <mslim1210@gmail.com>
2020-01-06 00:54:02 -0500

    destroy a weaker monster
    everything is working except the time that a weaker monster attacks a stronger monster and gets destroyed.

--- src/support/BattlePhase.java
+++ src/support/BattlePhase.java
@@ -12,8 +12,13 @@ import java.util.Map;
 public class BattlePhase extends Phase {
 
     private Card attacker;
+    private JPanel attackerImage;
+
     private Card victim;
+    private JPanel victimImage;
+
     private boolean letsFight;
+
 
 
     //싸운놈리시트
@@ -43,6 +48,7 @@ public class BattlePhase extends Phase {
                     //  entry.getValue().removeMouseListener(entry.getValue().getMouseListeners()[entry.getValue().getMouseListeners().length- 1]);
                     if (letsFight) {//공격선언을 해야 적의 몬스터들을 클릭했을때 전투를 실행하다.
                         victim = entry.getKey();
+                        victimImage = entry.getValue();
 
                         System.out.println(entry.getKey().getName() + "을/를 공격했습니다!");
 
@@ -57,6 +63,20 @@ public class BattlePhase extends Phase {
 
                                 System.out.println("플레이어 생존점수:" + player.getLp());
                                 System.out.println("공격한놈의 공격력이 맞은놈의 공격력보다 낮아 공격한놈이 파괴되었습니다.");
+
+                              //  player.getDfp().getCenter().goToCemetery(attacker, attackerImage);
+
+                                player.getDfp().getCenter().getMyFieldMonsterZone().get(attacker).remove(1);
+                                player.getDfp().getCenter().getMyFieldMonsterZone().get(attacker).repaint();
+                                player.getDfp().getCenter().getMyFieldMonsterZone().get(attacker).validate();
+
+
+                                //이거만손보면도ㅐ
+                                //지금안되는이유는 공격한놈 frame에서는 공격자 제거가 가능한데, 정작 맞은놈쪽에서 공격자의 이미지를 제거할 방법이없다.
+                                enemy.getDfp().getCenter().goToCemetery(attacker, attackerImage);
+
+
+
                             } else if (damage > 0) {
                                 //상대몬스터를 개팻을경우
                                 enemy.setLp(enemy.getLp() - damage);
@@ -64,9 +84,28 @@ public class BattlePhase extends Phase {
                                 enemy.getDfp().updatePlayerLp();
                                 System.out.println("적의 생존점수:" + enemy.getLp());
                                 System.out.println("공격한놈의 공격력이 맞은놈의 공격력보다 높아 맞은놈이 파괴되었습니다.");
+
+                                //자기 필드에서 맞은놈을 삭제
+                                player.getDfp().getCenter().goToCemetery(victim, victimImage);
+
+                                //적의 시야에 보이는 피해자의 몬스터를 삭제
+                                enemy.getDfp().getCenter().getMyFieldMonsterZone().get(victim).remove(1);
+                                enemy.getDfp().getCenter().getMyFieldMonsterZone().get(victim).repaint();
+                                enemy.getDfp().getCenter().getMyFieldMonsterZone().get(victim).validate();
+
+
                             } else {
                                 //같을경우
                                 System.out.println("공격한놈의 공격력이 맞은놈의 공격력과 같아 동반자살하였습니다.");
+
+
+                                player.getDfp().getCenter().goToCemetery(victim, victimImage);
+
+                                enemy.getDfp().getCenter().getMyFieldMonsterZone().get(victim).remove(1);
+
+
+                                enemy.getDfp().getCenter().goToCemetery(attacker, attackerImage);
+                                player.getDfp().getCenter().getMyFieldMonsterZone().get(attacker).remove(1);
                             }
                         } else { //수비표시로 존재하는 몬스터를 공격했을때.
                             int damage = ((Monster) (attacker)).getATK() - ((Monster) (victim)).getDEF();
@@ -85,8 +124,26 @@ public class BattlePhase extends Phase {
                                 container.repaint();
                                 container.validate();
 
-                                //여기서 새로만들거 헌정하자
                                 enemy.getDfp().getCenter().updateMyMonsterZone(entry.getKey(), reversedImage);
+                                victimImage = reversedImage;
+
+                                if (damage < 0) {
+                                    player.setLp(player.getLp() + damage);
+                                    player.getDfp().updatePlayerLp();
+                                    enemy.getDfp().updateEnemyLp();
+                                    System.out.println("플레이어 생존점수:" + player.getLp());
+                                    System.out.println("공격한놈의 공격력이 맞은놈의 수비력보다 낮아 공격한 놈의 컨트롤러가 그 수치만큼 데미지를 입습니다.");
+                                } else if (damage > 0) {
+                                    System.out.println("공격한놈의 공격력이 맞은놈의 수비력보다 높아 맞은놈이 파괴되었습니다.");
+
+                                    //공격자의 frame중 맞은놈을 삭제
+                                    player.getDfp().getCenter().goToCemetery(victim, victimImage);
+                                    //피해자의 frame중 맞은놈을 삭제
+                                    enemy.getDfp().getCenter().getMyFieldMonsterZone().get(victim).remove(1);
+                                } else {
+                                    //같을경우
+                                    System.out.println("공격한놈의 공격력이 맞은놈의 수비력과 같아 서로 아무 데미지도 입지않았습니다.");
+                                }
                             } else {
                                 if (damage < 0) {
                                     player.setLp(player.getLp() + damage);
@@ -96,6 +153,9 @@ public class BattlePhase extends Phase {
                                     System.out.println("공격한놈의 공격력이 맞은놈의 수비력보다 낮아 공격한 놈의 컨트롤러가 그 수치만큼 데미지를 입습니다.");
                                 } else if (damage > 0) {
                                     System.out.println("공격한놈의 공격력이 맞은놈의 수비력보다 높아 맞은놈이 파괴되었습니다.");
+
+                                    player.getDfp().getCenter().goToCemetery(victim, victimImage);
+                                    enemy.getDfp().getCenter().getMyFieldMonsterZone().get(victim).remove(1);
                                 } else {
                                     //같을경우
                                     System.out.println("공격한놈의 공격력이 맞은놈의 수비력과 같아 서로 아무 데미지도 입지않았습니다.");
@@ -119,10 +179,12 @@ public class BattlePhase extends Phase {
             entry.getValue().addMouseListener(new MouseAdapter() {
                 @Override
                 public void mouseClicked(MouseEvent e) {
+
                     super.mouseClicked(e);
 
                     System.out.println("하이");
                     attacker = entry.getKey();
+                    attackerImage = entry.getValue();
 
                     JPopupMenu pm = new JPopupMenu("선택창");
                     JMenuItem m1 = new JMenuItem("공격");
